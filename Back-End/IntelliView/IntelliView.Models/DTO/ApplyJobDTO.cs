afa4509f6f4883bf0b345efcb2cbc1525a497f70
Hassan Mahmoud <67936909+hassanmahmoud334@users.noreply.github.com>
2024-01-23 22:24:58 +0200

    Add JobApplication Controller

--- Back-End/IntelliView/IntelliView.Models/DTO/ApplyJobDTO.cs
+++ Back-End/IntelliView/IntelliView.Models/DTO/ApplyJobDTO.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace IntelliView.Models.DTO
@@ -9,7 +10,9 @@ namespace IntelliView.Models.DTO
     public class ApplyJobDTO
     {
         public string Id { get; set; }
+        [JsonIgnore]
         public string JobId { get; set; }
+        [JsonIgnore]
         public string IndividualUserId { get; set; }
         public string Status { get; set; } = "Pending";
     }

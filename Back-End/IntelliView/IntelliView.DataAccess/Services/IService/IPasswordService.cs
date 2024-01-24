483b277f34c7792d79f76f5e7f9b289307945aa5
Mohamed Nasr <mahamednasr6@gmail.com>
2024-01-24 17:52:43 +0200

    Add forget password create passwordService and forget password api

---
+++ Back-End/IntelliView/IntelliView.DataAccess/Services/IService/IPasswordService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliView.DataAccess.Services.IService
+{
+    public interface IPasswordService 
+    {
+        Task<bool> CheckEmailAsync(string email);
+        Task<string> CreateResetLink(string email);
+    }
+}
---
+++ Back-End/IntelliView/IntelliView.Utility/GenerateRandomToken.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliView.Utility
+{
+    public class GenerateRandomToken
+    {
+        public static string createRandomToken()
+        {
+            var token = new byte[32];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(token);
+            return Convert.ToBase64String(token);
+        }
+    }
+}

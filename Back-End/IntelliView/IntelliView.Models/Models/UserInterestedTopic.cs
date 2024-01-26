6145dea4bc7c545141563f5ac27cdd8e754c44be
osama salah <osama1salah861@gmail.com>
2024-01-26 22:41:44 +0200

    Adding Interested topic table

---
+++ Back-End/IntelliView/IntelliView.Models/Models/UserInterestedTopic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliView.Models.Models.job
+{
+    public class UserInterestedTopic
+    {
+        public int InterestedTopicId { get; set; }
+        public virtual InterestedTopic InterestedTopic { get; set; }
+        public string UserId { get; set; }
+        public virtual IndividualUser IndividualUser { get; set; }
+    }
+}

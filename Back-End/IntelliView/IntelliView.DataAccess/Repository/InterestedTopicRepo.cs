3dbb11abe4a33543189bd27197015fd9286017d7
osama salah <osama1salah861@gmail.com>
2024-01-28 02:14:18 +0200

    Adding data in Interested topic table adding repo for interested topic table and return all topics

---
+++ Back-End/IntelliView/IntelliView.DataAccess/Repository/InterestedTopicRepo.cs
@@ -0,0 +1,23 @@
+﻿using InteliView.DataAccess.Data;
+using IntelliView.DataAccess.Repository.IRepository;
+using IntelliView.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliView.DataAccess.Repository
+{
+    public class InterestedTopicRepo : Repository<InterestedTopic>, IInterestedTopicRepo
+    {
+        private ApplicationDbContext _db;
+        public InterestedTopicRepo(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(CompanyUser company)
+        {
+        }
+    }
+}
--- Back-End/IntelliView/IntelliView.Models/Models/job/JobInterestedTopic.cs
+++ Back-End/IntelliView/IntelliView.Models/Models/job/JobInterestedTopic.cs
@@ -1,13 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using IntelliView.Models.DTO;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IntelliView.Models.Models.job
 {
+    [PrimaryKey(nameof(InterestedTopicId), nameof(JobId))]
     public class JobInterestedTopic
     {
+        [Column(Order = 0)]
         public int InterestedTopicId { get; set; }
         [ForeignKey(nameof(InterestedTopicId))]
         public virtual InterestedTopic InterestedTopic { get; set; }
+        [Column(Order = 1)]
         public int JobId { get; set; }
+        [ForeignKey(nameof(JobId))]
         public virtual Job Job { get; set; }
+
     }
 }

3e271c27d6104776a2b602c34cd965b65590a0b6
Osama Elzekred <osamaelzekred@gmail.com>
2024-01-31 23:55:55 +0200

    response middleware

---
+++ Back-End/IntelliView/IntelliView.DataAccess/Middlewares/JsonResponseMiddleware.cs
@@ -0,0 +1,39 @@
+﻿
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+
+namespace IntelliView.DataAccess.Middlewares
+{
+
+    public class JsonResponseMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public JsonResponseMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            context.Response.OnStarting(state =>
+            {
+                var httpContext = (HttpContext)state;
+                httpContext.Response.Headers.Add("Content-Type", "application/json");
+                return Task.CompletedTask;
+            }, context);
+
+            await _next(context);
+        }
+    }
+
+    public static class JsonResponseMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseJsonResponseMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<JsonResponseMiddleware>();
+        }
+    }
+
+}

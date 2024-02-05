918579cfcaaa3c0440556d5b5bd46de304caf380
osama salah <osama1salah861@gmail.com>
2024-02-05 23:55:35 +0200

    add allowed extention jpg,jpeg,png for picture add extention pdf for cv add max size = 5 for pdf and picture

---
+++ Back-End/IntelliView/IntelliView.Utility/Settings/AllowedExtenionsAttribute.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliView.Utility.Settings
+{
+    public class AllowedExtenionsAttribute : ValidationAttribute
+    {
+        private readonly string _allowedExtensions;
+        public AllowedExtenionsAttribute(string allowedExtensions)
+        {
+            _allowedExtensions = allowedExtensions;
+        }
+        protected override ValidationResult? IsValid
+            (object? value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return ValidationResult.Success;
+            }
+            var result =validationContext.Items.
+                TryGetValue("AllowedExtensions", out object? allowedExtensions);
+            var file = value as IFormFile;
+            var extension = Path.GetExtension(file.FileName);
+            if (!_allowedExtensions.Contains(extension.ToLower()))
+            {
+                return new ValidationResult($"This file extension is not allowed!");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}

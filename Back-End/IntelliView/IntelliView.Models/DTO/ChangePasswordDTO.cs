a22385ae46b2fee69d7a5eb9f602f90fcbacf941
Mohamed Nasr <mahamednasr6@gmail.com>
2024-02-12 01:07:02 +0200

    Add change pass from profile

---
+++ Back-End/IntelliView/IntelliView.Models/DTO/ChangePasswordDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliView.Models.DTO
+{
+    public class ChangePasswordDTO
+    {
+        public string UserId { get; set; } = string.Empty;
+        public string OldPassword { get; set; } = string.Empty;
+        public string NewPassword { get; set; } = string.Empty;
+    }
+}

ea9b0ec74e9316774037bf93de25a0b07dc0e2da
Emara9 <emarahossam19@gmail.com>
2024-02-12 14:31:27 +0200

    add profile photo api

---
+++ Front-End/js/forgot-password.js
@@ -0,0 +1,34 @@
+
+
+let emailForm = document.getElementById('forgotPasswordForm'); 
+let messageError = document.getElementById("error");
+messageError.style.cssText = `
+    display : none ; 
+`
+emailForm.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+    let emailFormData = new FormData(emailForm); 
+    let email = emailFormData.get("email"); 
+    fetch("https://localhost:7049/api/Password/forget-password" , {
+        method : "POST", 
+        headers : {
+            "Content-type": "application/json; charset=UTF-8",
+    },
+    body : JSON.stringify({
+        email , 
+    }),
+    }).then((response)=> {
+        return response.json(); 
+    }).then((data) => {
+        if (data.status === 400){
+            messageError.textContent = "invalid email "; 
+            messageError.style.display = "block"; 
+        }
+        else {
+        // Hide the form
+        document.getElementById('forgotPasswordForm').style.display = 'none';
+        // Display the message
+        document.getElementById('message').style.display = 'block';
+        }
+    })
+});

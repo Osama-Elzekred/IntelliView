8411ced9437e735195cc33a8abdbba0e4639e3ac
Hassan Mahmoud <67936909+hassanmahmoud334@users.noreply.github.com>
2024-02-29 00:45:21 +0200

    add jobListDTO

---
+++ Back-End/IntelliView/IntelliView.Models/DTO/JobListDTO.cs
@@ -0,0 +1,21 @@
+﻿using IntelliView.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliView.Models.DTO
+{
+    public class JobListDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Location { get; set; } = string.Empty;
+        public JobType? Jobtype { get; set; }=JobType.OnSite;
+        public JobTime? Jobtime { get; set; }=JobTime.FullTime;
+        public string CompanyName { get; set; } = string.Empty;
+        public string CompanyLogo { get; set; } = @"wwwroot/Assets/images/7495e58b-b72b-4b87-8c12-c77a69b39cd3.jpg";
+
+    }
+}

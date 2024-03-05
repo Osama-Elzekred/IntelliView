ecdd421baed6a4af03ca323a4cc9ee84a34e4d4a
Hassan Mahmoud <67936909+hassanmahmoud334@users.noreply.github.com>
2024-03-05 02:53:01 +0200

    edit apply job

--- Front-End/app/job/[id]/apply/page.jsx
+++ Front-End/app/job/[id]/apply/page.jsx
@@ -57,7 +57,7 @@ function MainComponent() {
         </div>
         <div className="mb-4">
           <label>
-            Upload CV (File accepted: .pdf- Max file size: 5MB)
+            Upload CV (File accepted: .pdf - Max file size: 5MB)
           </label>
           <input
             name="resume"
@@ -65,11 +65,10 @@ function MainComponent() {
             type="file"
           />
         </div>
-        
         <div className="flex justify-end">
           <button
             type="button"
-            className="bg-[#1dbe72] text-white py-2 px-4 rounded hover:bg-[#17a162]"
+            className="bg-[#17a9c3] text-white py-2 px-4 rounded hover:bg-[#17a162]"
             onClick={handleNext}
           >
             Next
@@ -81,37 +80,13 @@ function MainComponent() {
 
   const StepTwo = (
     <div>
-      <h3 className="font-semibold mb-4">Experience</h3>
+      <h3 className="font-semibold mb-4 ">Answer  Questions</h3>
       <form>
         <label>Select your specialities</label>
         <input
           name="webDesign"
           className="w-full p-2 mb-4 border rounded"
           placeholder="Web Design"
-          type="text"
-        />
-        <input
-          name="illustration"
-          className="w-full p-2 mb-4 border rounded"
-          placeholder="Illustration"
-          type="text"
-        />
-        <input
-          name="uiUxDesign"
-          className="w-full p-2 mb-4 border rounded"
-          placeholder="UI/UX Design"
-          type="text"
-        />
-        <input
-          name="mobileDesign"
-          className="w-full p-2 mb-4 border rounded"
-          placeholder="Mobile Design"
-          type="text"
-        />
-        <input
-          name="otherSkills"
-          className="w-full p-2 mb-4 border rounded"
-          placeholder="Other Skills (Separated by commas)"
           type="text"
         />
         <div className="flex justify-between">
@@ -124,7 +99,7 @@ function MainComponent() {
           </button>
           <button
             type="button"
-            className="bg-[#1dbe72] text-white py-2 px-4 rounded hover:bg-[#17a162]"
+            className="bg-[#17a9c3] text-white py-2 px-4 rounded hover:bg-[#17a162]"
             onClick={handleNext}
           >
             Next
@@ -140,11 +115,10 @@ function MainComponent() {
         return StepOne;
       case 2:
         return StepTwo;
-      // More cases for additional steps can be added here
       default:
         return (
           <div>
-            <h3 className="font-semibold mb-4">Thank you abbass!</h3>
+            <h3 className="font-semibold mb-4 text-[#17a9c3]">Thank you!</h3>
             <p>Your application has been submitted.</p>
           </div>
         );
@@ -153,31 +127,51 @@ function MainComponent() {
 
   return (
     <div className="flex h-screen bg-[#f7f7f7]">
-      <div className="w-1/2 bg-[#1dbe72] p-10 text-white">
-        {/* Add the photo here */}
-        <img
-          src="/images/info_graphic_1.png"
-          alt="Photo"
-          className="h-100 object-contain"
-        />
-        {/* Unchanged left-side content */}
-      </div>
-      <div className="w-1/2 p-10">
-        <div className="float-right">
-          <i className="fa fa-bars fa-2x cursor-pointer"></i>
+      <div className="w-1/3 bg-[#17a9c3] p-10 text-white flex flex-col justify-between hidden sm:block">
+        <div>
+          <div className="flex justify-between">
+            <div className="footer-social">
+              <a href="#" className="mr-1">
+                <span className="icon-facebook" />
+              </a>
+              <a href="#" className="mr-1">
+                <span className="icon-twitter" />
+              </a>
+              <a href="#" className="mr-1">
+                <span className="icon-instagram" />
+              </a>
+              <a href="#" className="mr-1">
+                <span className="icon-linkedin" />
+              </a>
+            </div>
+            <div>
+              <i className="fa fa-bars fa-2x cursor-pointer"></i>
+            </div>
+          </div>
+          <div className="mt-12"></div>
         </div>
-        <div className="clear-both"></div>
+        <div className="flex flex-col justify-center items-center">
+          <img
+            src="/images/info_graphic_1.svg"
+            alt="image"
+            className="w-50 h-60"/>
+          <h1 className="font-bold text-4xl my-4">We are Hiring</h1>
+          <p className="mb-4">
+            please fill the form to apply for the job
+          </p>
+          <div className="text-sm mt-32">&copy; 2024 Intelliview</div>
+        </div>
+      </div>
+      <div className="w-3/3 p-10 flex justify-center lg:2/3 sm:w-2/3">
         <div className="mt-10">
-          <h2 className="text-[#1dbe72] text-2xl font-bold mb-4">
-            Presentation
+          <h2 className=" text-2xl font-bold mb-4">
+            Application Form
           </h2>
           {renderStep()}
         </div>
       </div>
     </div>
   );
-  
-  
 }
 
 export default MainComponent;

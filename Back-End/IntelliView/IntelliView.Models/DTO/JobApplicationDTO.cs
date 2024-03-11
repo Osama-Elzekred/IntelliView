31c867109a8acde530816f53dc2bc0367faeda27
Osama Elzekred <osamaelzekred@gmail.com>
2024-03-11 07:04:25 +0200

    fix apply job code (CV dosnt work)

--- Back-End/IntelliView/IntelliView.Models/DTO/JobApplicationDTO.cs
+++ Back-End/IntelliView/IntelliView.Models/DTO/JobApplicationDTO.cs
@@ -1,27 +1,15 @@
-﻿namespace IntelliView.Models.DTO
+﻿
+namespace IntelliView.Models.DTO
 {
-    public class JobApplicationDTO
+    public class JobApplicationDto
     {
         public int JobId { get; set; }
-
-        public string? UserId { get; set; }
-
-        public string FullName { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string Phone { get; set; } = string.Empty;
-        public string? ResumeURL { get; set; }
-
-        // Additional properties as needed for your DTO
-
-        // For example, you might include a collection of answers to job questions
-        public virtual List<UserJobAnswerDTO> UserAnswers { get; set; } = new List<UserJobAnswerDTO>();
-    }
-
-    public class UserJobAnswerDTO
-    {
-        public int QuestionId { get; set; }
-
-        public string? Answer { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Gender { get; set; }
+        public byte[]? CV { get; set; }
+        public string QuestionsAnswers { get; set; }
     }
 
 }

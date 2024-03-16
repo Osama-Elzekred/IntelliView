6ad52e7c9ab263f434dbbcce7bd843ed79ad0515
Hossam Emara <emarahossam19@gmail.com>
2024-03-17 01:41:26 +0200

    video interview page and post videos to the server

--- Front-End/app/Interview/Vedio-interview/[id]/page.jsx
+++ Front-End/app/Interview/Vedio-interview/[id]/page.jsx
@@ -1,12 +1,30 @@
-'use client';
-import React from 'react';
-import { StartInterview } from '../../../components/components';
+"use client";
+import React from "react";
+import { StartInterview } from "../../../components/components";
+import { useState, useRef ,useEffect} from "react";
+import { redirect } from "next/navigation";
+
 function MainComponent() {
+  const fullQuestionList = [
+    "What is your greatest strength?",
+    "Where do you see yourself in 5 years?",
+    "Why should we hire you?",
+    "what is the expected salary?"
+  ];
+  const questionVideos = [
+    "/images/vid1.mp4",
+    "/images/vid1.mp4",
+    "/images/vid1.mp4",
+  ];
   const [currentIndex, setCurrentIndex] = React.useState(0);
   const [isListVisible, setIsListVisible] = React.useState(true);
   const [isRecording, setIsRecording] = React.useState(false);
   const [recordingTime, setRecordingTime] = React.useState(0);
-
+  const [stream, setStream] = useState(null);
+  const videoRef = useRef(null);
+  const [recordedVideos, setRecordedVideos] = useState(Array(fullQuestionList.length).fill(null));  
+  const mediaRecorderRef = useRef(null);
+  const [userVideo, setUserVideo] = useState([]);
   React.useEffect(() => {
     let timer;
     if (isRecording) {
@@ -16,13 +34,7 @@ function MainComponent() {
     }
     return () => clearInterval(timer);
   }, [isRecording]);
-
-  const fullQuestionList = [
-    'What is your greatest strength?',
-    'Where do you see yourself in 5 years?',
-    'Why should we hire you?',
-  ];
-
+  const [qVideos, setQVideos] = useState(questionVideos[0]);
   const questionList = fullQuestionList.slice(0, currentIndex + 1);
   const [CurrentStep, setCurrentStep] = React.useState(1);
   const selectedQuestion = questionList[currentIndex];
@@ -30,27 +42,108 @@ function MainComponent() {
     fullQuestionList.length
   }`;
   const toggleListVisibility = () => setIsListVisible(!isListVisible);
-
-  const handleStartRecording = () => {
-    setIsRecording(true);
-    setRecordingTime(0);
+  
+  // start recording code 
+  const handleStartRecording = async () => {
+    try {
+      const mediaStream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+      });
+      setStream(mediaStream);
+      // Mute the audio track during streaming
+      mediaStream.getAudioTracks().forEach((track) => {
+        track.enabled = true;
+      });
+      videoRef.current.srcObject = mediaStream;
+      // Create a new MediaRecorder with video and audio stream
+      const mediaRecorder = new MediaRecorder(mediaStream, {
+        mimeType: "video/webm",
+      });
+      mediaRecorderRef.current = mediaRecorder;
+      setRecordingTime(0);
+      mediaRecorder.ondataavailable = (event) => {
+        if (event.data.size > 0) {
+          const blob = new Blob([event.data], { type: "video/webm" });
+          const newRecordedVideos = [...recordedVideos];
+          newRecordedVideos[currentIndex] = blob;
+          setRecordedVideos(newRecordedVideos);
+          const blobUrl = URL.createObjectURL(blob);
+          setUserVideo(blobUrl);
+          console.log(recordedVideos); 
+        }
+      };
+      mediaRecorder.start();
+      setIsRecording(true);
+    } catch (err) {
+      console.error("Error accessing camera:", err);
+    }
   };
-
+// stop recording code .... 
   const handleStopRecording = () => {
-    setIsRecording(false);
-  };
-
-  const handleNextQuestion = () => {
-    if (currentIndex < fullQuestionList.length - 1) {
-      setCurrentIndex((prevIndex) => prevIndex + 1);
+    const mediaRecorder = mediaRecorderRef.current;
+    if (mediaRecorder && mediaRecorder.state === "recording") {
+      mediaRecorder.stop();
+      const tracks = stream.getTracks();
+      tracks.forEach((track) => track.stop());
+      setIsRecording(false);
+      
     }
   };
 
+  //next button code... 
+  const recordedVideosLength = recordedVideos.filter(video => video !== null).length;
+  const handleNextQuestion = () => {
+    if (currentIndex < fullQuestionList.length - 1) {
+      setCurrentIndex((prevIndex) => prevIndex + 1);
+      setQVideos(questionVideos[currentIndex + 1]);
+      setRecordingTime(0)
+    }
+  };
+
+//prev button code....  
+  const handlePrevious = () => {
+    setCurrentIndex(currentIndex - 1);
+    setQVideos(questionVideos[currentIndex - 1]);
+    setRecordingTime(0)
+  };
+
+//post recorded videos 
+  const formData = new FormData();
+  recordedVideos.forEach((video, index) => {
+    formData.append(`video${index}`, video);
+  });
+    const handleUploadVideos = async () => {
+    try {
+
+      const response = await fetch("/upload-videos", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        console.log("Videos uploaded successfully");
+        redirect("/job/1"); 
+      } else {
+        console.error("Failed to upload videos");
+      }
+    } catch (error) {
+      console.error("Error uploading videos:", error);
+    }
+  };
+  useEffect(()=> {
+    if (recordingTime === 15 ){
+      handleStopRecording(); 
+      console.log("stop"); 
+    }
+  },[recordingTime])
+
   const minutes = Math.floor(recordingTime / 60);
   const seconds = recordingTime % 60;
-  const formattedTime = `${minutes.toString().padStart(2, '0')}:${seconds
+  const formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds
     .toString()
-    .padStart(2, '0')}`;
+    .padStart(2, "0")}`;
+
 
   const RenderStep = () => {
     switch (CurrentStep) {
@@ -69,11 +162,12 @@ function MainComponent() {
         return null;
     }
   };
+
   const InterviewStep = (
     <div className="flex flex-col lg:flex-row w-full">
       <div
         className={`bg-white overflow-hidden shadow-lg h-full transition-all ease-in-out duration-500 ${
-          isListVisible ? 'lg:w-2/5' : 'lg:w-0'
+          isListVisible ? "lg:w-2/5" : "lg:w-0"
         }`}
       >
         <h1
@@ -90,8 +184,8 @@ function MainComponent() {
                 key={question}
                 className={`p-4 cursor-pointer hover:bg-gray-100 ${
                   currentIndex === index
-                    ? 'bg-blue-100 border-l-4 border-blue-500 pl-2'
-                    : ''
+                    ? "bg-blue-100 border-l-4 border-blue-500 pl-2"
+                    : ""
                 }`}
                 onClick={() => setCurrentIndex(index)}
               >
@@ -104,7 +198,7 @@ function MainComponent() {
 
       <div
         className={`flex-grow p-4 lg:p-8 flex flex-col h-auto lg:h-full lg:max-w-full transition-all ease-in-out duration-500 ${
-          !isListVisible ? 'lg:max-w-full' : 'lg:max-w-3xl'
+          !isListVisible ? "lg:max-w-full" : "lg:max-w-3xl"
         }`}
       >
         <div className="flex items-center lg:items-start mb-4">
@@ -116,14 +210,20 @@ function MainComponent() {
           <div className="w-full lg:w-1/2 mb-4 lg:mb-0 lg:mr-4">
             <div className="relative h-full">
               <video
-                src="https://cvoiceprodwus2.blob.core.windows.net/batch-synthesis-output/e3d0f6ab-0358-410b-aed0-14bfc32dd828/0001.webm?skoid=85130dbe-2390-4897-a9e9-5c88bb59daff&sktid=33e01921-4d64-4f8c-a055-5bdaffd5e33d&skt=2024-03-14T00%3A14%3A38Z&ske=2024-03-20T00%3A19%3A38Z&sks=b&skv=2023-11-03&sv=2023-11-03&st=2024-03-14T00%3A14%3A38Z&se=2024-03-15T00%3A19%3A38Z&sr=b&sp=rl&sig=BgVJnxKXHuNU8blQlD2Cu1QJ4ZgBKlASY1W%2BYopLob0%3D"
                 className={`w-full rounded-lg shadow h-4/5 mb-2 border-4 ${
-                  isRecording ? 'border-red-500' : 'border-transparent'
+                  isRecording ? "border-red-500" : "border-transparent"
                 }`}
-                controls
+                width="1200"
+                height="900"
+                autoPlay
+                playsInline
+                muted
+                ref={videoRef}
+                  // poster="/Images/ai.jpg"
               ></video>
+
               <div className="flex justify-between items-center p-2">
-                {isRecording ? (
+                {isRecording === true ? (
                   <button
                     className="text-white font-roboto py-2 px-4 rounded-full bg-red-500"
                     onClick={handleStopRecording}
@@ -140,7 +240,7 @@ function MainComponent() {
                 )}
                 <span
                   className={`font-roboto text-lg rounded-full ${
-                    isRecording ? 'text-red-500' : 'text-green-500'
+                    isRecording ? "text-red-500" : "text-green-500"
                   }`}
                 >
                   {formattedTime}
@@ -151,10 +251,13 @@ function MainComponent() {
           <div className="w-full lg:w-1/2 mb-4 lg:mb-0">
             <video
               className={`w-full rounded-lg shadow mb-2 h-4/5 border-2 ${
-                isRecording ? 'border-green-600' : 'border-transparent'
+                isRecording ? "border-green-600" : "border-transparent"
               }`}
+              width="640"
+              height="480"
               controls
-              src={`path/to/interview-question-video-${currentIndex}.mp4`}
+              src={qVideos}
+              type="video/mp4"
             ></video>
           </div>
         </div>
@@ -162,21 +265,34 @@ function MainComponent() {
           <button
             className="bg-gray-300 text-gray-800 font-roboto py-2 px-4 rounded-l-lg"
             disabled={currentIndex <= 0}
-            onClick={() => setCurrentIndex(currentIndex - 1)}
+            onClick={handlePrevious}
           >
             Previous
           </button>
-          <button
-            className="bg-gray-300 text-gray-800 font-roboto py-2 px-4 rounded-r-lg"
-            disabled={currentIndex >= fullQuestionList.length - 1}
-            onClick={handleNextQuestion}
-          >
-            Next
-          </button>
+          {currentIndex < fullQuestionList.length - 1 ? (
+            <button
+              className="bg-gray-300 text-gray-800 font-roboto py-2 px-4 rounded-r-lg"
+              disabled = {isRecording || recordedVideosLength !== currentIndex + 1 }
+              onClick={() => {
+                handleNextQuestion();
+              }}
+            >
+              Next
+            </button>
+          ) : (
+            <button
+              className="bg-gray-300 text-gray-800 font-roboto py-2 px-4 rounded-r-lg"
+              onClick={handleUploadVideos}
+              disabled = {isRecording || recordedVideosLength !== currentIndex + 1}
+            >
+              Submit
+            </button>
+          )}
         </div>
       </div>
     </div>
   );
+
   return RenderStep();
 }
 

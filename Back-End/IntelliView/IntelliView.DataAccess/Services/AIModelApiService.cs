bf3758772eda08e3eaec6dae77e3ec4c69fa12fd
monasr6 <mahamednasr6@gmail.com>
2024-04-24 06:03:10 +0200

    Add service for ai api models

---
+++ Back-End/IntelliView/IntelliView.DataAccess/Services/AIModelApiService.cs
@@ -0,0 +1,55 @@
+﻿using IntelliView.DataAccess.Services.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace IntelliView.DataAccess.Services
+{
+    public class AIModelApiClient : IAIModelApiService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly JsonSerializerOptions _jsonSerializerOptions;
+
+        public AIModelApiClient()
+        {
+            _httpClient = new HttpClient();
+
+            // Configure JSON serializer options
+            _jsonSerializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            };
+        }
+
+        public async Task<object> SendRequestAsync(object dataToSend,string url,string apiKey)
+        {
+            // Serialize the input object to JSON
+            var jsonData = JsonSerializer.Serialize(dataToSend, _jsonSerializerOptions);
+
+            // Add authorization header with your API key
+            _httpClient.DefaultRequestHeaders.Add("Authorization", apiKey);
+
+            // Create a StringContent object with the JSON data to send
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            // Send a POST request with the JSON data in the request body
+            HttpResponseMessage response = await _httpClient.PostAsync(url, content);
+
+            // Check if the request was successful
+            if (response.IsSuccessStatusCode)
+            {
+                // Read and deserialize the response JSON object
+                var responseContent = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<object>(responseContent, _jsonSerializerOptions);
+            }
+            else
+            {
+                // Handle error response
+                return $"Error: {response.StatusCode}";
+            }
+        }
+    }
+}
---
+++ Back-End/IntelliView/IntelliView.DataAccess/Services/IService/IAIModelApiService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliView.DataAccess.Services.IService
+{
+    public interface IAIModelApiService
+    {
+        Task<object> SendRequestAsync(object dataToSend, string url, string apiKey);
+    }
+}

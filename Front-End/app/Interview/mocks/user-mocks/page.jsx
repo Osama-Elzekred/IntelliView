8272e06f018cd89a6836af9d9d403285d874d983
Hossam Emara <emarahossam19@gmail.com>
2024-05-04 21:42:09 +0300

    search in mocks page and arrow buttons (scroll)

--- Front-End/app/Interview/mocks/user-mocks/page.jsx
+++ Front-End/app/Interview/mocks/user-mocks/page.jsx
@@ -1,55 +1,59 @@
 "use client";
-import { redirect } from "next/dist/server/api-utils";
 import Layout from "../../../components/Layout";
 import MockCard from "../../../components/MockCard";
-import Link from "next/link";
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
 function UserMocks() {
+  const [searchState, setSearchState] = useState(false);
+  const [searchTerm, setSearchTerm] = useState();
+  const [searchResult, setSearchResult] = useState([]);
+  const [mocksData, setMocksData] = useState([]);
   const dataMocks = [
     {
       id: 1,
       icon: "aaa",
-      title: "BackEnd Developer",
-      topic: "Techno",
+      title: "Coach",
+      category: "Sports",
       description: " ay hewar kpeer wekhlas ahla klam bye men gheer salam ",
       score: 4.5,
     },
     {
       id: 2,
       icon: "aaa",
-      title: "BackEnd Developer",
-      topic: "Techno",
+      title: "Coach",
+      category: "Sports",
       description: " ay hewar kpeer wekhlas ahla klam bye men gheer salam ",
       score: 4.5,
     },
     {
       id: 3,
       icon: "aaa",
-      title: "BackEnd Developer",
-      topic: "Techno",
+      title: "FrontEnd Developer",
+      category: "techno",
       description: " ay hewar kpeer wekhlas ahla klam bye men gheer salam ",
       score: 4.5,
     },
     {
       id: 4,
       icon: "aaa",
-      title: "BackEnd Developer",
-      topic: "Techno",
+      title: "FrontEnd Developer",
+      category: "techno",
       description: " ay hewar kpeer wekhlas ahla klam bye men gheer salam ",
       score: 4.5,
     },
     {
       id: 5,
       icon: "aaa",
-      title: "BackEnd Developer",
-      topic: "Techno",
+      title: "Doctor ",
+      category: "Medicine",
       description: " ay hewar kpeer wekhlas ahla klam bye men gheer salam ",
       score: 4.5,
     },
     {
       id: 6,
       icon: "aaa",
-      title: "BackEnd Developer",
-      topic: "Techno",
+      title: "Coach",
+      category: "Sports",
       description: " ay hewar kpeer wekhlas ahla klam bye men gheer salam ",
       score: 4.5,
     },
@@ -57,7 +61,7 @@ function UserMocks() {
       id: 7,
       icon: "aaa",
       title: "BackEnd Developer",
-      topic: "Techno",
+      category: "Techno",
       description: " ay hewar kpeer wekhlas ahla klam bye men gheer salam ",
       score: 4.5,
     },
@@ -65,7 +69,7 @@ function UserMocks() {
       id: 8,
       icon: "aaa",
       title: "BackEnd Developer",
-      topic: "Techno",
+      category: "Techno",
       description: " ay hewar kpeer wekhlas ahla klam bye men gheer salam ",
       score: 4.5,
     },
@@ -73,7 +77,7 @@ function UserMocks() {
       id: 9,
       icon: "aaa",
       title: "BackEnd Developer",
-      topic: "Techno",
+      category: "Techno",
       description: " ay hewar kpeer wekhlas ahla klam bye men gheer salam ",
       score: 4.5,
     },
@@ -81,11 +85,50 @@ function UserMocks() {
       id: 10,
       icon: "aaa",
       title: "BackEnd Developer",
-      topic: "Techno",
+      category: "Techno",
       description: " ay hewar kpeer wekhlas ahla klam bye men gheer salam ",
       score: 4.5,
     },
   ];
+  useEffect(() => {
+    const fetchMocksData = async () => {
+      const authToken = Cookies.get("authToken");
+      try {
+        const response = await fetch("https://localhost:7049/api/Job/GetAll", {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        });
+        if (response.ok) {
+          const mocks = await response.json();
+          setMocksData(mocks);
+        }
+      } catch (error) {
+        console.log("error : ", error);
+      }
+    };
+    fetchMocksData();
+  }, [mocksData]);
+
+  const handleSearch = () => {
+    setSearchState(true);
+
+    if (!searchTerm) {
+      // If search term is empty, reset search state and clear search results
+      setSearchState(false);
+      setSearchResult([]);
+    } else {
+      // If search term is not empty, filter the data based on the search term
+      const filteredResults = dataMocks.filter(
+        (item) =>
+          item.category.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          item.title.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setSearchResult(filteredResults);
+      
+    }
+  };
   const redirectToReview = (userId) => {
     window.location.href = `/Interview/UserList/${userId}`;
   };
@@ -115,30 +158,93 @@ function UserMocks() {
                   <div className="mb-5 text-center">
                     <h3 className="font-bold text-white">Your Mocks</h3>
                   </div>
+
+                  <form
+                    className="search-jobs-form "
+                    onSubmit={(e) => {
+                      handleSearch();
+                      e.preventDefault();
+                      e.stopPropagation();
+                    }}
+                  >
+                    <div className="d-flex align-items-center justify-content-center">
+                      <input
+                        type="text"
+                        className="form-control mr-2"
+                        style={{ width: "500px" }}
+                        placeholder="Category-Title ...."
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                      />
+                      <button
+                        type="submit"
+                        className="btn-lg bg-[#17a9c3] hover:bg-[#2ebfd9] text-white btn-search flex justify-content-center align-items-center"
+                      >
+                        <span className="icon-search icon mr-2" />
+                      </button>
+                    </div>
+                  </form>
                 </div>
               </div>
             </div>
-            <Link href="#next" className="scroll-button smoothscroll">
-              <span className=" icon-keyboard_arrow_down" />
-            </Link>
+            <div
+              className="scroll-button smoothscroll"
+              style={{
+                position: "absolute",
+                bottom: "-26px",
+                left: "49.9%",
+                transform: "translateX(-50%)",
+                cursor: "pointer",
+              }}
+              onClick={() => {
+                window.scrollTo({
+                  top: document.body.scrollHeight,
+                  behavior: "smooth",
+                });
+              }}
+            >
+              <span className="icon-keyboard_arrow_down" />
+            </div>
           </section>
           <section>
-            <div className=" m-2 p-2 grid grid-cols-4 gap-4">
-              {dataMocks.map((mock) => (
-                <MockCard
-                  icon={mock.icon}
-                  title={mock.title}
-                  topic={mock.topic}
-                  description={mock.description}
-                  overallScore={mock.score}
-                  onClick={() => {
-                    redirectToReview(mock.id);
-                  }}
-                />
-              ))}
+            <div className=" m-2 mt-4 mb-4 p-2 grid grid-cols-4 gap-4">
+              {searchState === true && searchResult.length != 0 ? (
+                searchResult.map((mock) => (
+                  <MockCard
+                    icon={mock.icon}
+                    title={mock.title}
+                    category={mock.category}
+                    description={mock.description}
+                    overallScore={mock.score}
+                    onClick={() => {
+                      redirectToReview(mock.id);
+                    }}
+                  />
+                ))
+              ) : searchState === true && searchResult.length === 0 ? (
+                <div className=" d-flex align-items-center justify-content-center"> 
+                    <h4>
+                        No Mocks Found 
+                    </h4>
+                </div>
+              ) : (
+                dataMocks.map((mock) => (
+                  <MockCard
+                    icon={mock.icon}
+                    title={mock.title}
+                    category={mock.category}
+                    description={mock.description}
+                    overallScore={mock.score}
+                    onClick={() => {
+                      redirectToReview(mock.id);
+                    }}
+                  />
+                ))
+              )}
             </div>
           </section>
         </div>
+        <div id="footer"></div>
       </>
     </Layout>
   );

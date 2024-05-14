83101903ef86efbdb165bbc81f73604496c97ccd
Osama Elzekred <osamaelzekred@gmail.com>
2024-05-14 12:51:38 +0300

    chore: Remove unused search functionality from UserMocks page

--- Front-End/app/not-found.jsx
+++ Front-End/app/not-found.jsx
@@ -1,19 +1,38 @@
-import Link from "next/link";
-function NotFound(){ 
-return (
-<section className="flex items-center h-full p-16 dark:bg-gray-50 dark:text-gray-800">
-	<div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
-		<div className="max-w-md text-center">
-			<h2 className="mb-8 font-extrabold text-9xl dark:text-gray-400">
-				<span className="sr-only">Error</span>404
-			</h2>
-			<p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
-			<p className="mt-4 mb-8 dark:text-gray-600">But dont worry, you can find plenty of other things on our homepage.</p>
-			<Link rel="noopener noreferrer" href="/" className="px-8 py-3 font-semibold rounded dark:bg-violet-600 dark:text-gray-50">Back to homepage</Link>
-		</div>
-	</div>
-</section> 
-)
-} 
+import Link from 'next/link';
+function NotFound() {
+  return (
+    <>
+      <>
+        {/* This is an example component */}
+        <div className="h-screen w-screen bg-gray-50 flex items-center">
+          <div className="container flex flex-col md:flex-row items-center justify-between px-5 text-gray-700">
+            <div className="w-full lg:w-1/2 mx-8">
+              <div className="text-7xl text-primary font-dark font-extrabold mb-8">
+                {' '}
+                404 <br /> Not found
+              </div>
+              <p className="text-2xl md:text-3xl font-light leading-normal mb-8">
+                Sorry we couldn't find the page you're looking for
+              </p>
+              <a
+                href="/"
+                className="px-5 inline py-3 text-sm font-medium leading-5 shadow-2xl text-white transition-all duration-400 border border-transparent rounded-lg focus:outline-none bg-primary active:bg-red-600 hover:bg-red-700"
+              >
+                back to homepage
+              </a>
+            </div>
+            <div className="w-full lg:flex lg:justify-end lg:w-1/2 mx-5 my-12">
+              <img
+                src="https://user-images.githubusercontent.com/43953425/166269493-acd08ccb-4df3-4474-95c7-ad1034d3c070.svg"
+                className=""
+                alt="Page not found"
+              />
+            </div>
+          </div>
+        </div>
+      </>
+    </>
+  );
+}
 
-export default NotFound ; 
+export default NotFound;

30422ca3cb0e1bfc7f6f463ab6b7a3b196ecd4ec
Osama Elzekred <osamaelzekred@gmail.com>
2024-06-24 05:26:04 +0300

    adding the Aicharts

--- Front-End/app/components/AiCharts/AIcharts.jsx
+++ Front-End/app/components/AiCharts/AIcharts.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
 import EmotionScoresChart from './EmotionScoresChart';
 import SimilarityScoreChart from './SimilarityScoreChart';
+import { Rating, Card } from 'flowbite-react';
 
 const AICharts = ({ aiScores }) => {
   return (
-    <div>
-      <EmotionScoresChart emotionScores={aiScores.emotionScores} />
-      <SimilarityScoreChart similarityScore={aiScores.answerSimilarityScore} />
+    <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
+      {/* Adjust flex direction and spacing for different screen sizes */}
+      <Card className="w-full sm:w-3/4 md:min-w-[80%] md:flex-3 lg:w-3/4 xl:w-3/4">
+        <EmotionScoresChart data={aiScores.emotionScores} />
+      </Card>
+      <div className="flex flex-col space-y-1 w-full sm:w-1/4">
+        <Card className="p-4 w-full">
+          {/* Adjust width for larger screens to control the size of the cards */}
+          <SimilarityScoreChart
+            similarityScore={aiScores.answerSimilarityScore}
+          />
+        </Card>
+        <Card className="mb-1 text-sm font-bold text-gray-900 dark:text-white">
+          {/* Display sentiment score */}
+          <h5 className="text-lg font-bold justify-center items-center text-center">
+            Sentiment Score
+          </h5>
+          <div className="flex flex-grow justify-center items-center text-center">
+            <div>
+              <Rating>
+                <Rating.Star />
+                <p className="ml-2 text-sm font-bold text-gray-900 dark:text-white">
+                  {aiScores.sentimentScore}
+                </p>
+              </Rating>
+            </div>
+          </div>
+        </Card>
+      </div>
     </div>
   );
 };
--- Front-End/app/components/FullScreenModal.jsx
+++ Front-End/app/components/FullScreenModal.jsx
@@ -31,35 +31,48 @@ const FullScreenModal = ({ onClose, rowData }) => {
   }, []);
 
   return (
-    <div className="fixed top-[64px] right-0 w-2/3  bg-black bg-opacity-50 z-50  h-full ">
+    <div className="fixed top-[64px] right-0 w-3/4  bg-black bg-opacity-50 z-50  h-full ">
       <div className="bg-slate-50 rounded-lg p-8  h-full overflow-y-auto  pb-60">
         <div className="flex justify-end">
-          <div className="h-5 w-5 cursor-pointer" onClick={onClose}>
+          <div className="h-5 w-5 cursor-pointer m-1" onClick={onClose}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
               <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
             </svg>
           </div>
         </div>
         <h2 className="text-xl font-semibold mb-4">{title}</h2>
-        {/* Video Element */}
-        <div className="relative flex justify-center items-center">
-          <video src={videoUrl} width="640" height="480" controls autoPlay loop>
-            {' '}
-          </video>
+        <div className="">
+          <AIcharts aiScores={aiObjects} />
         </div>
-        {/* Video Details */}
-        <div className="mt-4">
-          <p className="text-lg font-semibold mb-2">AI Score: {aiRating}</p>
-          <p className="text-lg font-semibold mb-2">Comments(notes) :{notes}</p>
-          <p className="text-lg font-semibold mb-2">Recorded Date: {date}</p>
-          <p className="text-lg font-semibold mb-2">
-            Question: {InterviewQuestion?.Question}
-          </p>
-          <p className="text-lg font-semibold mb-2">
-            Model Answer: {InterviewQuestion?.answer}
-          </p>
+        <div className="flex flex-col md:flex-row m-2 ">
+          {/* Video Element */}
+          <div className="flex-1 flex justify-center items-center md:mr-4">
+            <video
+              src={videoUrl}
+              width="640"
+              height="480"
+              controls
+              autoPlay
+              loop
+              className="rounded-md"
+            ></video>
+          </div>
+          {/* Video Details */}
+          <div className="flex-1 mt-4 md:mt-0">
+            <p className="text-lg font-semibold mb-2">AI Score: {aiRating}</p>
+            <p className="text-lg font-semibold mb-2">
+              Comments(notes) :{notes}
+            </p>
+            <p className="text-lg font-semibold mb-2">Recorded Date: {date}</p>
+            <p className="text-lg font-semibold mb-2">
+              Question: {InterviewQuestion?.Question}
+            </p>
+            <p className="text-lg font-semibold mb-2">
+              Model Answer: {InterviewQuestion?.answer}
+            </p>
+          </div>
         </div>
-        <AIcharts aiScores={aiObjects} />
+
         {/* Close Button */}
       </div>
     </div>

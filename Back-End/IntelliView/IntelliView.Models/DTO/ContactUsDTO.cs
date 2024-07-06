484d34bb2c24617f2a8ff3baf23519d3e58d1670
monasr6 <mahamednasr6@gmail.com>
2024-07-06 23:45:12 +0300

    Add contact us api

---
+++ Back-End/IntelliView/IntelliView.Models/DTO/ContactUsDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace IntelliView.API.Controllers
+{
+    public class ContactUsDTO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
